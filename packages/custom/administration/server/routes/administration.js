5f75b1761295eee7ea413a2be526c0f706a22431
qtrandev <qtrandev@gmail.com>
2015-03-21 19:36:42 -0400

    Layout features of the administration package and add buttons

--- packages/custom/administration/server/routes/administration.js
+++ packages/custom/administration/server/routes/administration.js
@@ -4,17 +4,43 @@
 // The Package is past automatically as first parameter
 module.exports = function(Administration, app, auth, database) {
 
-  app.get('/administration/example/anyone', function(req, res, next) {
-    res.send('Anyone can access this');
+  app.get('/administration/adoptions', auth.requiresLogin, function(req, res, next) {
+    res.send('Page with list of all adoptions will be displayed');
   });
 
-  app.get('/administration/example/auth', auth.requiresLogin, function(req, res, next) {
-    res.send('Only authenticated users can access this');
+  app.get('/administration/reports', auth.requiresLogin, function(req, res, next) {
+    res.send('Page with list of all reports will be displayed');
   });
 
+  app.get('/administration/users', auth.requiresLogin, function(req, res, next) {
+    res.send('Interface to manage all users will be displayed');
+  });
+
+  app.get('/administration/email', auth.requiresLogin, function(req, res, next) {
+    res.send('Interface to email users will be displayed');
+  });
+
+  app.get('/administration/reportsdue', auth.requiresLogin, function(req, res, next) {
+    res.send('Interface to show adoptions with reports being due');
+  });
+
+  app.get('/administration/adoptionsexpire', auth.requiresLogin, function(req, res, next) {
+    res.send('Interface to show adoptions expiring soon');
+  });
+
+  app.get('/administration/streets', auth.requiresLogin, function(req, res, next) {
+    res.send('Interface to show all adopted and undopted streets');
+  });
+
+  app.get('/administration/backup', auth.requiresLogin, function(req, res, next) {
+    res.send('Interface to allow all data to be backed up - spreadsheets or zip files');
+  });
+
+  /**
   app.get('/administration/example/admin', auth.requiresAdmin, function(req, res, next) {
     res.send('Only users with Admin role can access this');
   });
+  */
 
   app.get('/administration/example/render', function(req, res, next) {
     Administration.render('index', {
